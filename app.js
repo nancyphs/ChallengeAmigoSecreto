d4f4477a0c8e46e53b6253e823e19566e43a0a7f
Nancy Hernandez <nancyphs26@gmail.com>
2025-03-16 19:39:40 -0600

    feat: Agrega amigos al array

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Se declara el array
+let amigos =[];
+
+// Funcion para agregar amigo
+function agregarAmigo(){
+    const añadir = document.getElementById("amigo"); // Obtiene el texto que el usuario escribe
+    const nombre = añadir.value.trim(); // Elimina espacios extra
+
+    if (nombre) {
+        amigos.push(nombre); // Agrega al array
+        actualizarLista();
+        console.log("Nombre agregado:", nombre);
+        console.log("array:", amigos);
+        añadir.value = ""; //Se limpia el campo de entrada
+        añadir.focus(); //Vuelve al input
+    }
+
+
+}
+
+
